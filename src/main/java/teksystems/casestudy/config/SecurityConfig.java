f94ef093c98810322f02051ec711c66d846541e8
Joshua_Johnson <joshualj2018@gmail.com>
2022-04-16 15:03:47 -0500

    "Implemented frontend security"

--- src/main/java/teksystems/casestudy/config/SecurityConfig.java
+++ src/main/java/teksystems/casestudy/config/SecurityConfig.java
@@ -27,9 +27,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
             .csrf().disable()
             .authorizeRequests()
                 //allows a user to access each of these folders
-                .antMatchers("/pub/**", "/error/**", "/login/**", "/index", "/user/**").permitAll()
+                .antMatchers("/pub/**", "/error/**", "/login/**", "/index").permitAll()
                 //every controller should have its own URL -- like /user/ or /appointment/  //URLs are a protected resource for anyone
-                .antMatchers("/clinician/**").authenticated()
+                .antMatchers("/clinician/**","/user/**").authenticated()
                 .and()
             .formLogin()
                 .loginPage("/login/login") //this is a URL
