84edaed4f9ada1b3aa9cb38fdaa755c04080a652
Joshua_Johnson <joshualj2018@gmail.com>
2022-04-21 11:02:28 -0500

    "Committing FileUpload"

---
+++ src/main/java/teksystems/casestudy/config/FileUploadConfig.java
@@ -0,0 +1,9 @@
+package teksystems.casestudy.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class FileUploadConfig {
+
+}
